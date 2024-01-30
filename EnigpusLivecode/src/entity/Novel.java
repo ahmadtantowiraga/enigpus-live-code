98bff127649c8afa721eefa2326c5a41c978dfaa
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-01-30 08:48:53 +0700

    [FEATURE] Add Novel Class

---
+++ EnigpusLivecode/src/entity/Novel.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Novel extends Book{
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public void getTitle() {
+
+    }
+}
