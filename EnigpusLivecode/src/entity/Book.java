e01198cfff94b7318990c0d9adbf217aa29f207b
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-01-30 09:04:10 +0700

    [IMPROVE] Improve property and constarctor in Book Class

--- EnigpusLivecode/src/entity/Book.java
+++ EnigpusLivecode/src/entity/Book.java
@@ -1,6 +1,15 @@
 package entity;
 
 public abstract class Book {
+    private String title;
+    private int publicationYear;
+    private String code;
+
+    public Book(String title, int publicationYear) {
+        this.title = title;
+        this.publicationYear = publicationYear;
+    }
+
     public abstract String getCode();
     public abstract void getTitle();
 }
