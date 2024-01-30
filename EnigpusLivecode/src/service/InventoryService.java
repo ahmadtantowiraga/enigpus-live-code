6b412cee3d0ed650723edd5b1784e3fcff715398
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-01-30 09:21:12 +0700

    [FEATURE] Add InvenntoryService Interface

---
+++ EnigpusLivecode/src/service/InventoryService.java
@@ -0,0 +1,9 @@
+package service;
+
+public interface InventoryService {
+    public void addBook();
+   public void searchBookByTitle();
+   public void searchBookByCode();
+   public void deleteBookByCode();
+   public void getAllBook();
+}
