75f151b26af0e36527e1ec7f5cd02364dca9fcb8
ahmad Tanto Wiraga <ahmadtantowiraga@gmail.com>
2024-01-30 08:50:35 +0700

    [FEATURE] Add Magazine Class

---
+++ EnigpusLivecode/src/entity/Magazine.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Magazine extends Book{
+
+    @Override
+    public String getCode() {
+        return null;
+    }
+
+    @Override
+    public void getTitle() {
+
+    }
+}
